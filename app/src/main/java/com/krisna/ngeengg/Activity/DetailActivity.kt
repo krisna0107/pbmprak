731514dab81b14991fbb67c347c76b68dce45af1
krisna0107 <krisnayanajavista@gmail.com>
2019-12-24 03:05:07 +0700

    ly date

--- app/src/main/java/com/krisna/ngeengg/Activity/DetailActivity.kt
+++ app/src/main/java/com/krisna/ngeengg/Activity/DetailActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.krisna.ngeengg.R
 import kotlinx.android.synthetic.main.activity_detail.*
 import ru.slybeaver.slycalendarview.SlyCalendarDialog
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -17,29 +18,20 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val c = Calendar.getInstance()
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
 
         mulai.setOnClickListener {
-//            val dialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
-//                jumlah.text = "$mDay/$mMonth/$mYear"
-//            }, year, month, day)
-//            dialog.show()
             callback = object : SlyCalendarDialog.Callback {
                 override fun onCancelled() {
 
                 }
 
                 override fun onDataSelected(firstDate: Calendar?, secondDate: Calendar?,  hours: Int, minutes: Int) {
-                    jumlah.text = firstDate.toString()
+                    if (firstDate != null) {
+                        jumlah.text = SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(firstDate.time)
+                    }
                 }
             }
-            SlyCalendarDialog()
-                .setSingle(false)
-                .setCallback(callback)
-                .show(supportFragmentManager, "TAG_SLYCALENDAR")
+            SlyCalendarDialog().setSingle(false).setCallback(callback).show(supportFragmentManager, "TAG_KRISNAYANAJAVISTA")
         }
     }
 }
