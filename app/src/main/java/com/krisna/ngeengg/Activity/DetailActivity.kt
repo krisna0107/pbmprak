2dba85db8a6baa4ed2cb184d3975368aca089b56
krisna0107 <krisnayanajavista@gmail.com>
2019-12-23 15:00:40 +0700

    date

--- app/src/main/java/com/krisna/ngeengg/Activity/DetailActivity.kt
+++ app/src/main/java/com/krisna/ngeengg/Activity/DetailActivity.kt
@@ -1,14 +1,18 @@
 package com.krisna.ngeengg.Activity
 
-import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.krisna.ngeengg.R
 import kotlinx.android.synthetic.main.activity_detail.*
+import ru.slybeaver.slycalendarview.SlyCalendarDialog
 import java.util.*
+
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var callback: SlyCalendarDialog.Callback
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -19,10 +23,23 @@ class DetailActivity : AppCompatActivity() {
         val day = c.get(Calendar.DAY_OF_MONTH)
 
         mulai.setOnClickListener {
-            val dialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
-                jumlah.setText(""+ mDay +"/"+ mMonth +"/"+mYear)
-            }, year, month, day)
-            dialog.show()
+//            val dialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+//                jumlah.text = "$mDay/$mMonth/$mYear"
+//            }, year, month, day)
+//            dialog.show()
+            callback = object : SlyCalendarDialog.Callback {
+                override fun onCancelled() {
+
+                }
+
+                override fun onDataSelected(firstDate: Calendar?, secondDate: Calendar?,  hours: Int, minutes: Int) {
+                    jumlah.text = firstDate.toString()
+                }
+            }
+            SlyCalendarDialog()
+                .setSingle(false)
+                .setCallback(callback)
+                .show(supportFragmentManager, "TAG_SLYCALENDAR")
         }
     }
 }
